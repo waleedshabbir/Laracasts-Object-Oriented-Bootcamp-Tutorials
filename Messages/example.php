3297b97f769bc5a16eb02d2c10008a0786a9d116
Waleed Shabbir <waleedshabbir1@gmail.com>
2016-09-22 12:54:46 +0500

    Messages between classes Completed

---
+++ Messages/example.php
@@ -0,0 +1,69 @@
+<?php
+
+
+class Person{
+
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
+}
+
+class Business{
+
+    protected $staff;
+
+    public function __construct(Staff $staff)
+    {
+       $this->staff=$staff;
+    }
+
+    public function hire(Person $person)
+    {
+        // add $person to the staff collection
+        $this->staff->add($person);
+    }
+
+    public function getStaffMembers()
+    {
+        return $this->staff->members();
+    }
+
+}
+
+class Staff{
+
+    protected $members=[];
+
+    public function __construct($members = [])
+    {
+        $this->members=$members;
+    }
+
+    public function add(Person $person)
+    {
+        $this->members[]=$person;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+
+}
+
+
+$waleed = new Person('Waleed Shabbir');
+
+$staff = new Staff([$waleed]);
+
+
+$laracasts = new Business($staff);
+
+//$laracasts->hire($waleed);
+$laracasts->hire(new Person('ABC '));
+
+
+var_dump($laracasts->getStaffMembers());
